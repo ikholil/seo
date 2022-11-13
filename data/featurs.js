b9be48596529733dbab9006c4526590d9430249c
ikholil <ikholil.bd@gmail.com>
2022-11-13 14:57:26 +0600

    changed img path

--- data/featurs.js
+++ data/featurs.js
@@ -1,29 +1,29 @@
 import icon1 from '../public/images/grow/icon1.png'
 import icon2 from '../public/images/grow/icon2.png'
 import icon3 from '../public/images/grow/icon3.png'
-import icon_bg1 from '../public/images/grow/icon-bg1.png'
-import icon_bg2 from '../public/images/grow/icon-bg2.png'
-import icon_bg3 from '../public/images/grow/icon-bg3.png'
+import icon_bg1 from '../public/images/icon-bg1.png'
+import icon_bg2 from '../public/images/icon-bg2.png'
+import icon_bg3 from '../public/images/icon-bg3.png'
 
 export const features = [
     {
         id: 1, 
         icon: icon1,
-        bg:icon_bg1,
+        bg:'/images/grow/icon-bg1.png',
         title:'Link Building',
         desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Convallis mattis dui eget vestibulum id et. It’s about ranking'
     },
     {
         id: 2, 
         icon: icon2,
-        bg:icon_bg2,
+        bg:'/images/grow/icon-bg2.png',
         title:'Optimization',
         desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Convallis mattis dui eget vestibulum id et. It’s about ranking'
     },
     {
         id: 3, 
         icon: icon3,
-        bg:icon_bg3,
+        bg:'/images/grow/icon-bg3.png',
         title:'SEO',
         desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Convallis mattis dui eget vestibulum id et. It’s about ranking'
     },
