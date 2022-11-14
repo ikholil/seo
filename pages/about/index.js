a37a146de091d90283b806578e325bf17d97af05
ikholil <ikholil.bd@gmail.com>
2022-11-14 14:18:13 +0600

    fixing case homepage

--- pages/about/index.js
+++ pages/about/index.js
@@ -2,7 +2,7 @@ import About from "../../components/About/About";
 import Banner from "../../components/About/Banner";
 import Team from "../../components/About/Team";
 import Clients from "../../components/About/ClientsLove";
-import Testimonial from "../../components/homepage/Testimonial/Testimonial";
+import Testimonial from "../../components/Homepage/Testimonial/Testimonial";
 
 const index = () => {
   return (
