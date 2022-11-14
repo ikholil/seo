b21ff175d499343ae09134a6cf6da03b1e7fbc71
ikholil <ikholil.bd@gmail.com>
2022-11-14 13:00:26 +0600

    import error fixed

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,15 +1,13 @@
-import Footer from './footer/Footer';
-import Navbar from './navbar/Navbar'
-const Layout = ({children}) => {
-    return (
-        <>
-           <Navbar /> 
-           <main>
-            {children}
-           </main>
-           <Footer />
-        </>
-    );
+import Navbar from '../components/navbar/Navbar'
+import Footer from '../components/footer/Footer'
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-
-import Layout from '../components/Layout'
-import '../styles/globals.css'
-import "slick-carousel/slick/slick.css"; 
+import "../styles/globals.css";
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Layout from '../components/Layout.jsx'
 function MyApp({ Component, pageProps }) {
-  return <Layout>
-    <Component {...pageProps} />
-  </Layout>
-    
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
